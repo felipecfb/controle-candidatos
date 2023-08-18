b2c423507ea6ab3b7d1e33052aa011375d0c3ef8
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-08-18 09:04:05 -0300

    feat: creating a function to return selected candidates

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -4,7 +4,23 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
-		selecaoCandidatos();
+		imprimirSelecionados();
+	}
+	
+	static void imprimirSelecionados() {
+		String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
+		
+		System.out.println("Imprimindo a lista de candidatos informando o indice do elemento.");
+		
+		for (int indice = 0; indice < candidatos.length; indice++) {
+			System.out.println("O candidato de nº " + (indice + 1) + " é o " + candidatos[indice]);
+		}
+		
+		System.out.println("Forma abrevida de interação for each");
+		
+		for (String candidato: candidatos) {
+			System.out.println("o candidato selecionado foi " + candidato);
+		}
 	}
 	
 	static void selecaoCandidatos() {
@@ -16,8 +32,6 @@ public class ProcessoSeletivo {
 		while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
 			String candidato = candidatos[candidatosAtual];
 			double salarioPretendido = valorPretendido();
-			
-			System.out.println("O candidato " + candidato + " Solicitou este valor de salário " + salarioPretendido);
 			
 			if (salarioBase >= salarioPretendido) {
 				System.out.println("O candidato " + candidato + " foi selecionado para a vaga.");
